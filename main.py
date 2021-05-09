0c7153c903594cfd86fd12adf0fc375a58bdc34c
Dydy2412 <Dydy2412@users.noreply.github.com>
2021-05-09 22:51:30 +1100

    Update 0.0.0.1a
    -Limit the number of char in Entries
    -Enchancements of the code

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 import tkinter.font as tkFont
 from sudo_lib import displayer
+from time import time
+from uuid import uuid1
 
 TITLE = 'SudoDydy'
 
@@ -12,8 +14,8 @@ class App(tk.Frame):
 
         self.helv36 = tkFont.Font(self, family='Helvetica', size=36, weight='bold')
 
-        self.sudo = Sudoku(self)
-        self.buttons = Commands(self)
+        self.sudo = Sudoku(self, self.helv36)
+        self.buttons = Commands(self, font=self.helv36, sudoku=self.sudo)
 
         self.load_widget()
 
@@ -22,73 +24,90 @@ class App(tk.Frame):
 
 class Sudoku(tk.Frame):
 
-    def __init__(self, master=None):
+    def __init__(self, master=None, font=None):
         super().__init__(master=master)
         self.pack()
 
         self.sudo_input = None
+        self.font = font
 
         self.load_widget()
 
     def load_widget(self):
-        self.sudo_input = [[Patch(self,i,j) for i in range(3)]for j in range(3)]
+        self.sudo_input = [[Patch(self, i,j, self.font) for i in range(3)]for j in range(3)]
 
-    def set_input(self, x,y):
-        pass
+    def set_input(self, x, y, data):
+        self.get_var(x,y).set(data)
 
     def get_sudo(self):
         return self.sudo_input
 
+    def get_cell(self,x,y):
+        return self.sudo_input[x//3][y//3].get_patchs()[x%3][y%3]
+
+    def get_var(self, x, y):
+        return self.get_cell(x, y).var
+
 class Patch(tk.Frame):
 
-    def __init__(self, master=None, i=0, j=0):
+    def __init__(self, master=None, i=0, j=0, font=None):
         super().__init__(master=master, borderwidth=3, relief='sunken')
         self.grid(column=i, row=j)
 
+        self.font = font
         self.stringVars = None
         self.patch_input = None
 
         self.load_widget()
 
     def load_widget(self):
-        self.stringVars = [[tk.StringVar(self) for i in range(3)] for j in range(3)]
-        self.patch_input = [[tk.Entry(self, justify='center', width = 2, font=self.master.master.helv36, borderwidth=2, relief='groove', textvariable=self.stringVars[i][j]) for i in range(3)] for j in range(3)]
-        for i in range(3):
-            for j in range(3):
-                print(self.stringVars[i][j])
-                self.stringVars[i][j].trace('w', self.on_input)
-                self.patch_input[i][j].grid(column = i, row = j)
+        self.patch_input = [[SudoEntry(self, justify='center', width = 2, font=self.font, borderwidth=2, relief='groove', row=i, column=j) for i in range(3)] for j in range(3)]
 
-    def on_input(self, *args):
-        var = self.getvar(args[0])
-        print(var.get())
+    def on_input(self, var_name):
+        print(self.getvar(var_name))
 
     def get_patchs(self):
         return self.patch_input
 
 class Commands(tk.Frame):
 
-    def __init__(self, master=None, i=0, j=0):
+    def __init__(self, master=None, font=None, sudoku=None):
         super().__init__(master=master, bg='#000')
         self.pack(fill=tk.BOTH)
+
+        self.font = font
+        self.sudoku = sudoku
 
         self.load_widget()
 
     def load_widget(self):
 
         self.columnconfigure(0, weight=1)
-        Gen_Button = tk.Button(self, text='Generate', font=self.master.helv36, relief='groove', borderwidth=3)
+        Gen_Button = tk.Button(self, text='Generate', font=self.font, relief='groove', borderwidth=3)
         Gen_Button.grid(column=0, row=0, sticky='nsew')
 
         self.columnconfigure(1, weight=1)
-        Verif_Button = tk.Button(self, text='Check', font=self.master.helv36, relief='groove', borderwidth=3, command=self.on_check)
+        Verif_Button = tk.Button(self, text='Check', font=self.font, relief='groove', borderwidth=3, command=self.on_check)
         Verif_Button.grid(column=1, row=0, sticky='nsew')
 
     def on_check(self):
-        print(self.master.sudo.get_sudo()[0][0].get_patchs()[0][0].get())
+        self.sudoku.set_input(0, 0, '5')
+
+class SudoEntry(tk.Entry):
+
+    def __init__(self, master=None, font=None, justify=None, width = 1, borderwidth=0, relief=None, row=0, column=0):
+        super().__init__(master=master, justify=justify, width = width, font=font, borderwidth=borderwidth, relief=relief)
+        self.grid(row=row, column=column)
+
+        self.var = tk.StringVar(self)
+        self.config(textvariable=self.var)
+        self.var.trace_add('write', lambda *args : self.on_write(self.var))
+
+    def on_write(self, var):
+        if len(var.get()) > 0:
+            var.set(var.get()[:1])
 
 root = tk.Tk()
 app = App(root)
 app.master.title(TITLE)
-# app.master.resizable(False, False)
 app.mainloop()
